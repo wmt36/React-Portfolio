91b21d5f990c7ef6e129816c604d4acc16f64b29
wmt36 <wmt36@Williams-iMac.local>
2020-11-21 14:34:27 -0700

    updated push for deployment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import NavBar from '../src/components/Navbar'
 import Home from '../src/components/Home'
-import Wrapper from '../src/components/Wrapper'
 import Container from '../src/components/Container'
 import About from '../src/components/pages/About'
 import Portfolio  from '../src/components/pages/Portfolio'
