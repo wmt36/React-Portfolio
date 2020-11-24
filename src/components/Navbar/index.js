8baaafbb3549802b2b90239acfcb51d9b97e142a
wmt36 <65432950+wmt36@users.noreply.github.com>
2020-11-24 10:11:52 -0700

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -15,12 +15,6 @@ function Navbar() {
       <li class="nav-item active">
         <a class="nav-link" href="/about">About</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/portfolio">Portfolio</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/contact">Contact</a>
-      </li>
     </ul>
   </div>
 </nav>
